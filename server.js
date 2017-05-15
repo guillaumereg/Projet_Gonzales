0f7b461bfb1f3b803e41c1eda22cd3f20bb8e744
QuentinHauspie <quentin.hauspie@student.uclouvain.be>
2017-05-15 19:04:47 +0100

    Upate README.md server.js

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ let express=require('express');
 var app=express();
 
 app.listen(8080,function(){
-    console.log('hello, world !');
+    console.log('Server running on port 8080');
 });
+
+app.get('/', function(req,res)
+            {
+                res.send("Hello World from server.js");
+            });
 
