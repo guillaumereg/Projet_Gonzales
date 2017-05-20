12e69326fc33e11bd1b364d323fd011d970ce2a7
guillaume <guillaume.neirinckx@student.uclouvain.be>
2017-05-20 18:39:51 +0200

    backup

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express'),
 	app		= express();
 
-app.get('/', function(req,res){ //=define routes, when someone makes aget request to root directory, respond by sending file named index.html
+app.get('/', function(req,res){ //=define routes, when someone makes a get request to root directory, respond by sending file named index.html
 	res.sendFile(__dirname + '/index.html');
 });
+
+//app.use('/js',express.static(__dirname + '/client/js')); //anytime user requests /js, use helper function = reply with client/js
 
 app.listen(8000, function(){ //listen on port 8000 and handle requests on it.
 	console.log('Je vous écoute...');
